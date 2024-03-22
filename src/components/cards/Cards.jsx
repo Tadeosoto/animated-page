061f87908030dcf5ae1f7a5de099d7758f6b24f8
Tadeosoto <tadeosoto1993@gmail.com>
2024-03-22 13:01:23 -0600

    se quitaron las etiquetas de servicios

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -25,7 +25,7 @@ const Cards = () => {
             <CardItem
               src="https://images.pexels.com/photos/6862189/pexels-photo-6862189.jpeg"
               text="Redes Eléctricas Aéreas y Subterráneas en Alta, Media y Baja Tensión"
-              label="Obra Civil y Electromécanica"
+              // label="Obra Civil y Electromécanica"
               path="/servicios"
             />
           </ul>
@@ -33,19 +33,19 @@ const Cards = () => {
             <CardItem
               src="./torreTC.jpg"
               text="Torres de telecomunicaciones"
-              label="Obra Civil y Electromécanica"
+              // label="Obra Civil y Electromécanica"
               path="/servicios"
             />
             <CardItem
               src="./focos.jpg"
               text="Instalaciones eléctricas de baja tensión"
-              label="Obra civil"
+              // label="Obra civil"
               path="/servicios"
             />
             <CardItem
               src="./tablero2.jpg"
               text="Tableros de Distribución y Control"
-              label="Obra Electromécanica"
+              // label="Obra Electromécanica"
               path="/servicios"
             />
           </ul>
@@ -53,25 +53,25 @@ const Cards = () => {
             <CardItem
               src="./sistemaTierra.jpg"
               text="Sistemas de Tierras y Pararrayos"
-              label="Obra Civil y Electromécanica"
+              // label="Obra Civil y Electromécanica"
               path="/servicios"
             />
             <CardItem
               src="https://images.unsplash.com/photo-1673268256087-7383f2ab84a2"
               text="Alumbrado"
-              label="Obra Civil"
+              // label="Obra Civil"
               path="/servicios"
             />
             <CardItem
               src="./vozyDatos.jpg"
               text="Instalaciones de voz y datos"
-              label="Conectividad"
+              // label="Conectividad"
               path="/servicios"
             />
             <CardItem
               src="https://images.pexels.com/photos/3760532/pexels-photo-3760532.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               text="Servicios Generales de Ingenería, Construcción y Proyectos ejecutivos de Obra eléctrica"
-              label="Planeación"
+              // label="Planeación"
               path="/servicios"
             />
           </ul>
