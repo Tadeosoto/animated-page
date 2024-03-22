9bd1e42a267c8827edaa694001fa0bfb84ffa458
Tadeosoto <tadeosoto1993@gmail.com>
2024-03-22 12:37:46 -0600

    cambio en los labels de servicios

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -13,19 +13,19 @@ const Cards = () => {
             <CardItem
               src="https://images.pexels.com/photos/207541/pexels-photo-207541.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               text="Líneas de Transmisión, Subtransmisión y Distribución"
-              label="Construccion"
+              label="Obra Civil y Electromécanica"
               path="/servicios"
             />
             <CardItem
               src="./subestacionElectrica.jpg"
               text="Subestaciones Eléctricas"
-              label="Construccion"
+              label="Obra Civil y Electromécanica"
               path="/servicios"
             />
             <CardItem
               src="https://images.pexels.com/photos/6862189/pexels-photo-6862189.jpeg"
               text="Redes Eléctricas Aéreas y Subterráneas en Alta, Media y Baja Tensión"
-              label="Construccion"
+              label="Obra Civil y Electromécanica"
               path="/servicios"
             />
           </ul>
@@ -33,19 +33,19 @@ const Cards = () => {
             <CardItem
               src="./torreTC.jpg"
               text="Torres de telecomunicaciones"
-              label="Construccion"
+              label="Obra Civil y Electromécanica"
               path="/servicios"
             />
             <CardItem
               src="./focos.jpg"
               text="Instalaciones eléctricas de baja tensión"
-              label="Construccion"
+              label="Obra civil"
               path="/servicios"
             />
             <CardItem
               src="./tablero2.jpg"
               text="Tableros de Distribución y Control"
-              label="Construccion"
+              label="Obra Electromécanica"
               path="/servicios"
             />
           </ul>
@@ -53,25 +53,25 @@ const Cards = () => {
             <CardItem
               src="./sistemaTierra.jpg"
               text="Sistemas de Tierras y Pararrayos"
-              label="Construccion"
+              label="Obra Civil y Electromécanica"
               path="/servicios"
             />
             <CardItem
               src="https://images.unsplash.com/photo-1673268256087-7383f2ab84a2"
               text="Alumbrado"
-              label="Construccion"
+              label="Obra Civil"
               path="/servicios"
             />
             <CardItem
               src="./vozyDatos.jpg"
               text="Instalaciones de voz y datos"
-              label="Construccion"
+              label="Conectividad"
               path="/servicios"
             />
             <CardItem
               src="https://images.pexels.com/photos/3760532/pexels-photo-3760532.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               text="Servicios Generales de Ingenería, Construcción y Proyectos ejecutivos de Obra eléctrica"
-              label="Construccion"
+              label="Planeación"
               path="/servicios"
             />
           </ul>
